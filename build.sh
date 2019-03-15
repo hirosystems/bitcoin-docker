42d24c474340d702c5d23cc9f767f4f2f0aed11b
wileyj <wileyj@users.noreply.github.com>
2019-03-15 15:43:26 -0400

    typo in docker image tag

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 echo "Building Dockerfile-gcp"
-docker build -f Dockerfile-gcp -t quay.io/blockstack/bitcoind:python.gcp -t bitcoind:python.gcp .
+docker build -f Dockerfile-gcp -t quay.io/blockstack/bitcoind:python.gcp -t blockstack/bitcoind:python.gcp .
 docker push quay.io/blockstack/bitcoind:python.gcp
 docker push blockstack/bitcoind:python.gcp
 echo ""
@@ -14,6 +14,6 @@ echo ""
 echo "Building Dockerfile-bitcoind.debian"
 docker build -f Dockerfile-bitcoind.debian -t quay.io/blockstack/bitcoind:debian -t blockstack/bitcoind:debian .
 docker push quay.io/blockstack/bitcoind:debian
-docker push blockstack/bitcoind:alpine
+docker push blockstack/bitcoind:debian
 echo "Done"
 echo ""
