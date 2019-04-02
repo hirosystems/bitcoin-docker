ae2199fc4703d6af11f1683fe3d3bda456fb6eba
wileyj <wileyj@users.noreply.github.com>
2019-04-02 14:02:34 -0400

    pushing all changes
    adding in bitcore

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-echo "Building Dockerfile-gcp"
-docker build -f Dockerfile-gcp -t quay.io/blockstack/bitcoind:python.gcp -t blockstack/bitcoind:python.gcp .
+echo "Building Dockerfile-python.gcp"
+docker build -f Dockerfile-python.gcp -t quay.io/blockstack/bitcoind:python.gcp -t blockstack/bitcoind:python.gcp .
 docker push quay.io/blockstack/bitcoind:python.gcp
 docker push blockstack/bitcoind:python.gcp
 echo ""
@@ -15,5 +15,15 @@ echo "Building Dockerfile-bitcoind.debian"
 docker build -f Dockerfile-bitcoind.debian -t quay.io/blockstack/bitcoind:debian -t blockstack/bitcoind:debian .
 docker push quay.io/blockstack/bitcoind:debian
 docker push blockstack/bitcoind:debian
+echo ""
+echo ""
+echo "Building Bitcore"
+git clone https://github.com/bitpay/bitcore .
+cp -R Dockerfile-bitcore bitcore/Dockerfile-bitcore.alpine
+cd bitcore
+docker build -f Dockerfile-bitcore.alpine -t quay.io/blockstack/bitcoind:bitcore -t blockstack/bitcoind:bitcore .
+docker push quay.io/blockstack/bitcoind:bitcore
+docker push blockstack/bitcoind:bitcore
+cd .. && rm -rf bitcore
 echo "Done"
 echo ""
