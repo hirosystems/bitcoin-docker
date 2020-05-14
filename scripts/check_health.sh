285422ffb4f482caa8363b343e76fe07788a8124
charlie <charlie@blockstack.com>
2020-05-14 18:23:14 -0400

    Only restart bitcore if threshold is passed and blocks aren't catching up

--- scripts/check_health.sh
+++ scripts/check_health.sh
@@ -54,7 +54,7 @@ function log_error() {
 # Create counter file for tracking the number of times bitcore has been observed as behind the bitcoin blockchain
 function create_counter_file() {
     TEMP_COUNTER_FILE=$(mktemp /tmp/${TEMP_COUNTER_FILE_BASE_NAME}.XXXXX)
-    echo "0" > ${TEMP_COUNTER_FILE}
+    echo -e "times failed,# of blocks behind\n0,0" > ${TEMP_COUNTER_FILE}
 }
 
 # Get the tmp counter file to use
@@ -74,26 +74,38 @@ function get_counter_file() {
     fi
 }
 
-# Get blockchain heights. Iterate the counter file if the heights don't match. Otherwise, reset the counter to 0
+# Get blockchain heights
+# Iterate the counter file if the heights don't match
+# Update the number of blocks bitcore is behind bitcoin_core
+# Otherwise, reset the counter to 0
 function compare_heights() {
     local bitcoin_core_height=$(docker logs ${BITCOIN_CORE_CONTAINER} --tail 100 | grep "height=" | cut -d '=' -f3 | awk '{print $1}' | tail -1)
     local bitcore_height=$(docker logs ${BITCORE_CONTAINER} --tail 100 | grep "height=" | cut -d '=' -f4 | tail -1)
+    local times_failed=$(cat ${TEMP_COUNTER_FILE} | tail -n 1 | cut -d',' -f1)
+    local blocks_behind_on_previous_run=$(cat ${TEMP_COUNTER_FILE} | tail -n 1 | cut -d',' -f2)
+    local blocks_behind_on_this_run=$((${bitcoin_core_height} - ${bitcore_height}))
 
     # Compare blockchain heights
     if [ "${bitcoin_core_height}" != "${bitcore_height}" ]; then
         # Increment counter file by 1
         log_info "Height discrepancy found. Incrementing counter file."
-        echo $(awk '{printf($1+1)}' ${TEMP_COUNTER_FILE}) > ${TEMP_COUNTER_FILE}
-    elif [ "${bitcoin_core_height}" = "${bitcore_height}" ]; then
-        # Reset counter file to 0
-        log_info "Heights match. Setting counter file to 0"
-        echo "0" > ${TEMP_COUNTER_FILE}
-    fi
+        sed -ri "s/([[:digit:]]),([[:digit:]])/$((${times_failed} + 1)),\2/g" ${TEMP_COUNTER_FILE}
 
-    # Restart Bitcore docker container
-    if [ $(cat ${TEMP_COUNTER_FILE}) -ge ${RESTART_THRESHOLD} ]; then
-        log_info "Threshold met. Restarting Bitcore Docker container..."
-        docker restart ${BITCORE_CONTAINER}
+        # Compare current blocks behind against previous blocks behind
+        # If the blocks are getting further behind bitcoin_core since the last execution, update the counter file
+        if [ ${blocks_behind_on_this_run} -gt ${blocks_behind_on_previous_run} ]; then
+            sed -ri "s/([[:digit:]]),([[:digit:]])/\1,${blocks_behind_on_this_run}/g" ${TEMP_COUNTER_FILE}
+
+            # Restart Bitcore docker container if where past the failure threshold
+            if [ $(cat ${TEMP_COUNTER_FILE}) -ge ${RESTART_THRESHOLD} ]; then
+                log_info "Threshold met and we're falling further behind in blocks than previous execution. Restarting Bitcore Docker container..."
+                docker restart ${BITCORE_CONTAINER}
+            fi
+        fi
+    elif [ "${bitcoin_core_height}" = "${bitcore_height}" ]; then
+        # Reset counter file to 0 runs failed and 0 blocks behind
+        log_info "Heights match. Setting counter file to 0"
+        sed -ri "s/([[:digit:]]),([[:digit:]])/0,0/g" ${TEMP_COUNTER_FILE}
     fi
 }
 
